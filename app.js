521756a8e0657b4b614e13694ce68f38c97bca09
吴金锋 <jinfeng.wu@edaibu.net>
2018-08-14 16:04:11 +0800

    添加日志功能

--- app.js
+++ app.js
@@ -6,35 +6,33 @@ const compress = require('koa-compress');
 const koaBody = require('koa-body');
 const koaStatic = require('koa-static');
 const favicon = require('koa-favicon');
+const onerror = require('koa-onerror');
+const log = require('./utils/log');
 
 const allRoutes = require('./routes/index');
 
 const app = new koa();
 app.use(logger((str, args) => {
-
+    log.info(args.slice(1).join(','));
 }));
 app.use(helmet());
 app.use(compress());
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(koaStatic(__dirname + '/public', {
-    // maxage: 600000 // 浏览器缓存十分钟
-}));
+app.use(koaStatic(__dirname + '/public'));
 app.use(views(__dirname + '/views', {
-    map: {
-        html: 'ejs'
-    },
+    map: {html: 'ejs'}, // 注意如果这样配置的话  {html:'ejs'} 模板的后缀名是.html
     extension: 'ejs'
 }));
 
 app.use(koaBody());
 
-app.keys = ['koa oms secret'];
+onerror(app);
 
 allRoutes(app);
 
 app.on('error', (err, ctx) => {
-    console.log('app err:', err, ctx);
+    console.error('app err:', err, ctx);
 });
 let port = 3000
 /* istanbul ignore next */
